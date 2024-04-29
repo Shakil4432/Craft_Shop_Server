a1bf691a1c2c081274e0b568efa6e5e5bba96631
Shakil4432 <shakil4432@gmail.com>
2024-04-29 12:56:53 +0600

    added update method

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
@@ -7,7 +8,6 @@ const port = process.env.PORT || 5000;
 app.use(express.json());
 app.use(cors());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER_ID}:${process.env.USER_PASS}@cluster0.q9r8zjr.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -23,6 +23,38 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const craftCollection = client.db('craftDB').collection('craft');
+
+    app.get('/JuteAndWoodenCraft',async(req,res)=>{
+        const cursor = craftCollection.find();
+        const result = await cursor.toArray();
+        res.send(result);
+    })
+
+    app.get('/JuteAndWoodenCraft/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)};
+        const result = await craftCollection.findOne(query);
+        res.send(result);
+    })
+
+    app.post('/JuteAndWoodenCraft',async(req,res)=>{
+        const craft = req.body;
+        const result = await craftCollection.insertOne(craft);
+        res.send(result);
+    })
+
+    app.delete('/JuteAndWoodenCraft/:id',async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: new ObjectId(id)}
+        const result = await craftCollection.deleteOne(query);
+        res.send(result)
+    })
+
+    
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
@@ -31,7 +63,7 @@ async function run() {
     // await client.close();
   }
 }
-run().catch(console.dir);
+run().catch(console.log);
 
 
 app.get('/',(req,res)=>{
